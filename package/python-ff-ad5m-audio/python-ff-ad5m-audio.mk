6e412f1d7538b8cd8266e8c6e6ed73d34aab8d47
consp <none@yourbusiness.com>
2024-03-01 22:50:22 +0100

    Added ff-ad5m-audio and mido as python package

---
+++ package/python-ff-ad5m-audio/python-ff-ad5m-audio.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# python-ff-ad5m-audio
+#
+################################################################################
+
+PYTHON_FF_AD5M_AUDIO_VERSION = 0.2
+PYTHON_FF_AD5M_AUDIO_SOURCE = ff-ad5m-audio-$(PYTHON_FF_AD5M_AUDIO_VERSION).tar.gz
+PYTHON_FF_AD5M_AUDIO_SITE = https://files.pythonhosted.org/packages/ac/46/17c26e0c1742127c486ac3ca7ddc349f628b0b58734afdb5dbf6fc03e9d2
+PYTHON_FF_AD5M_AUDIO_SETUP_TYPE = setuptools
+PYTHON_FF_AD5M_AUDIO_LICENSE = CC-4.0-BY-NC
+PYTHON_FF_AD5M_AUDIO_LICENSE_FILES = LICENSE
+
+$(eval $(python-package))
