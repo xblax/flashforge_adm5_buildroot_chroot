358318f18fc193476427075f1637e4c32c4e8a7d
consp <none@yourbusiness.com>
2024-03-08 12:05:40 +0100

    Version bump of audio module to v0.4

--- package/python-ff-ad5m-audio/python-ff-ad5m-audio.mk
+++ package/python-ff-ad5m-audio/python-ff-ad5m-audio.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_FF_AD5M_AUDIO_VERSION = 0.2
+PYTHON_FF_AD5M_AUDIO_VERSION = 0.4
 PYTHON_FF_AD5M_AUDIO_SOURCE = ff-ad5m-audio-$(PYTHON_FF_AD5M_AUDIO_VERSION).tar.gz
 PYTHON_FF_AD5M_AUDIO_SITE = https://files.pythonhosted.org/packages/ac/46/17c26e0c1742127c486ac3ca7ddc349f628b0b58734afdb5dbf6fc03e9d2
 PYTHON_FF_AD5M_AUDIO_SETUP_TYPE = setuptools
